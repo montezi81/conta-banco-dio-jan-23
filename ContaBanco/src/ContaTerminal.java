90b312d5d11e9e237e263c08863e4cdd3659b112
Marcos Costa <mvmontezi@gmail.com>
2023-01-06 14:11:25 -0300

    Meu Projeto Conta Banco

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor digite o número da agência: ");
+        
+
+
+    }
+}
